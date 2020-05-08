172f5a63151ae4ecffa1b1b3fed466d4ce86f9a8
Amscray <leo.calis@me.com>
2020-05-08 07:08:23 +0200

    delete pokerep

--- app/src/main/java/com/example/monprojet/data/PokeCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.monprojet.data;
-
-import com.example.monprojet.presentation.model.RestPokemonResponse;
-
-public interface PokeCallback {
-
-    public  void onSucess(RestPokemonResponse response);
-    public  void onFailure();
-}
